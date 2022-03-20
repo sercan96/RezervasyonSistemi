30a2c3fd5cb53046cb64071f3d3bd737510f3893
sercan1123 <sercan_saglam19@hotmail.com>
2022-03-20 19:44:18 +0300

    Rezervasyon Sistemi

---
+++ QUIZ2/Passenger.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUIZ2
+{
+    class Passenger
+    {
+        private int _seatNooo;
+        private string _anyClasses;
+        private string _nameandSurname;
+
+        public string NameSurname
+        {
+            get => _nameandSurname;
+            set => _nameandSurname = value;
+           
+        }
+
+        public string anyClass {
+
+            get => _anyClasses;
+            set
+            {
+                if(value == "1")
+                {
+                    _anyClasses = "EconomyClass";
+                }
+                else if (value == "2")
+                {
+                    _anyClasses = "BusinessClass";
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen 2 sayidan birini seçiniz.");
+                }
+            }
+        }
+
+        public int seatNo
+        {
+            get
+            {
+                return _seatNooo;
+            }
+            set
+            {      
+                if(_anyClasses == "EconomyClass")
+                {
+                    if (Program.EconomyClass.ContainsKey(value) && Program.EconomyClass[value] != "")
+                    {
+                        Program.Failed(value, this.NameSurname);
+                    }
+                    else if (!Program.EconomyClass.ContainsKey(value))
+                    {
+                        Program.FailedValue();
+                    }
+                    else
+                    {
+                        _seatNooo = value;
+                    }
+                }
+                else if(_anyClasses == "BusinessClass")
+                {
+                    if (Program.BusinessClass.ContainsKey(value) && Program.BusinessClass[value] != "")
+                    {
+                        Program.Failed(value, this.NameSurname);
+                    }
+                    else if (!Program.BusinessClass.ContainsKey(value))
+                    {
+                        Program.FailedValue();
+                    }
+                    else
+                    {
+                        _seatNooo = value;
+                    }
+                }
+               
+               
+            }
+        }
+      
+    }
+}
---
+++ QUIZ2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QUIZ2
+{
+    class Program
+    {
+        public static Dictionary<int, string> EconomyClass = new Dictionary<int, string>();
+
+        public static Dictionary<int, string> BusinessClass = new Dictionary<int, string>();
+        public static int choseValue;
+        public static Passenger psg;
+        static void Main(string[] args)
+        {
+            EmptySeatList();
+            ChoiceClass();  
+        }
+        static void EmptySeatList()
+        {
+            EconomyClass.Add(1, "");
+            EconomyClass.Add(2, "");
+            EconomyClass.Add(3, "");
+            EconomyClass.Add(4, "");
+            EconomyClass.Add(5, "");
+            EconomyClass.Add(6, "");
+            EconomyClass.Add(7, "");
+            EconomyClass.Add(8, "");
+            EconomyClass.Add(9, "");
+
+
+            BusinessClass.Add(9, "selin");
+            BusinessClass.Add(12, "");
+            BusinessClass.Add(13, "");
+            BusinessClass.Add(19, "sercan");
+            BusinessClass.Add(20, "");
+            BusinessClass.Add(24, "");
+            BusinessClass.Add(27, "");
+            BusinessClass.Add(30, "");
+            BusinessClass.Add(35, "");
+            BusinessClass.Add(40, "");
+            BusinessClass.Add(45, "");
+            BusinessClass.Add(50, "");
+        }
+       
+        static void ChoiceClass()
+        {
+            Console.WriteLine("Economy Class için 1'e basın");
+            Console.WriteLine("Business Class için 2'e basın");
+            psg = new Passenger();
+            switch (Convert.ToInt32(Console.ReadLine()))
+            {
+                case 1:
+                    psg.anyClass = "1";
+                    ControlEmptySeats(EconomyClass, "EconomyClass için boş olan koltuklar:");
+                    choseValue = Convert.ToInt32(Console.ReadLine());
+                    psg.seatNo = choseValue;
+                    Console.WriteLine("Lütfen Ad Soyad giriniz");
+                    psg.NameSurname = (Console.ReadLine().ToString());
+                    Console.WriteLine(psg.anyClass + " bölümündeki " + psg.seatNo + " numaralı koltuk " + psg.NameSurname + " isimli yolcuya rezerve edilmiştir ");
+                    EconomyClass[psg.seatNo] = "bu koltuk " + psg.NameSurname + " tarafından alındı, Lütfen başka bir koltuk seçiniz!";
+                    Console.WriteLine("Devam etmek için 0'a basın! ");
+                    AddNewPassengers();
+                    break;
+                case 2:
+                    psg.anyClass = "2";
+                    ControlEmptySeats(BusinessClass, "BusinessClass için boş olan koltuklar.");
+                    psg.seatNo = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Lütfen Ad Soyad giriniz;");
+                    psg.NameSurname = (Console.ReadLine().ToString());
+                    Console.WriteLine(psg.anyClass + " bölümündeki " + psg.seatNo + " numaralı koltuk " + psg.NameSurname + " isimli yolcuya rezerve edilmiştir. ");
+                    BusinessClass[psg.seatNo] = "bu koltuk " + psg.NameSurname + " tarafından alındı, Lütfen başka bir koltuk seçiniz!";
+                    Console.WriteLine("Devam etmek için 0'a basın! ");
+                    AddNewPassengers();
+                    break;
+            }
+        }
+
+        static void AddNewPassengers()
+        {
+            if (Convert.ToInt32(Console.ReadLine()) == 0)
+            {
+                ChoiceClass();
+            }
+        }
+        
+        static void ControlEmptySeats(Dictionary<int, string> seatList, string name)
+        {
+            Console.WriteLine(name);
+            foreach (var seats in seatList)
+            {
+                if (seats.Value == "")
+                {
+                    Console.WriteLine(seats.Key);
+                }
+                
+            }
+        }
+        
+        public static void Failed(int value, string name)
+        {
+            Console.WriteLine("Seçilen koltuk numarası : " + value);
+            Console.WriteLine(value + " numaralı koltuk başka bir yolcuya zaten rezerve edilmiş.  Lütfen başka bir koltuk seçiniz.");
+            ChoiceClass();
+        }
+        public static void FailedValue()
+        {
+            Console.WriteLine("Böyle bir değer yok");
+            ChoiceClass();
+        }
+    }
+}
